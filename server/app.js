88ce4cff7ceeec9114a0029234592c932af065da
Eric Tsai <hiphop200199@gmail.com>
2023-05-16 02:34:58 +0800

    setting backend

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ const Record = require('./models/Record');
 const cors = require('cors');
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const dbURI = 'mongodb+srv://eric:hiphop200199@cluster0.yqpvasi.mongodb.net/playboy-go-away?retryWrites=true&w=majority'
 mongoose.connect(dbURI,{
@@ -32,4 +32,4 @@ app.get('/read-records',(req,res)=>{
     Record.find().then(data=>res.json(data)).catch(err=>console.log(err))
 })
 
-app.listen(3001);
+app.listen(443);
